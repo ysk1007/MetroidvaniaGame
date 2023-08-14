cf96c7ca8b366aeb1b6162bdb624060f5cd6be61
OuxtHm <blue29856@naver.com>
2023-08-14 22:50:53 +0900

    Modifying MainCamera
    메인 카메라가 보스 몬스터를 비출 때 줌 기능 추가와 줌 기능에 의한 position 값 조절(현재 카메라가 이동할 수 있는 범위가 한정되어 y 축은 이동을 제대로 못 함, 추후 수정 예정)

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -7,9 +7,13 @@ public class MoveCamera : MonoBehaviour
 {
     [SerializeField] private Transform[] Boss;  // 보스오브젝트 (2023-08-14추가)
     [SerializeField] private GameObject[] bossName;  // 보스오브젝트 (2023-08-14추가)
+    [SerializeField] private GameObject UI;     // InGameUI 
 
-    [SerializeField] private bool startFightBoss = false;   // 보스전 시작시 true 되었다가 false로 변환
+    public bool startFightBoss = false;   // 보스전 시작시 true 되었다가 false로 변환
     public int stage;   // 추후 추가되는 MapManager에서 변수 가져오면 됨.
+
+    float changeX;
+    float changeY;
 
     public Transform target;
     public float speed;
@@ -18,6 +22,7 @@ public class MoveCamera : MonoBehaviour
     public Vector2 size;
     float height;
     float width;
+    public float screenSize;
 
     void Start()
     {
@@ -30,7 +35,7 @@ public class MoveCamera : MonoBehaviour
         if (startFightBoss)
         {
             Invoke("bossNameOn", 1.5f);
-            WathcingBoss();
+            WatchingBoss();
             Invoke("bossNameOff", 3f);
             Invoke("SituationTerminated", 3.5f);
         }
@@ -46,6 +51,7 @@ public class MoveCamera : MonoBehaviour
     void bossNameOff()  // 보스 이름 텍스트 내리는 함수
     {
         bossName[stage].gameObject.SetActive(false);
+        UI.SetActive(true); // InGameUI On
     }
 
     void SituationTerminated()  // 보스시작 종료
@@ -53,8 +59,9 @@ public class MoveCamera : MonoBehaviour
         startFightBoss = false;
     }
 
-    void WatchingPlayer()
+    void WatchingPlayer()   // 플레이어 비추는 카메라 함수
     {
+        Camera.main.orthographicSize = 9f;
         transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
 
         float lx = size.x * 0.5f - width;
@@ -66,9 +73,26 @@ public class MoveCamera : MonoBehaviour
         transform.position = new Vector3(clampX, clampY, -10f);
     }
 
-    void WathcingBoss()
+    void WatchingBoss()     // 보스 비추는 카메라 함수
     {
-        transform.position = Vector3.Lerp(transform.position, Boss[stage].position, Time.deltaTime * speed);
+        UI.SetActive(false);    // InGameUI Off
+
+        switch (stage)
+        {
+            case 0:
+                Camera.main.orthographicSize = 5f;
+                changeX = 4f;
+                changeY = 5.15f;
+                break;
+            case 1:
+                Camera.main.orthographicSize = 3f;
+                changeX = 0f;
+                changeY = -6f;
+                break;
+        }
+
+        Vector3 vector3 = new Vector3(changeX, changeY);
+        transform.position = Vector3.Lerp(transform.position, Boss[stage].position + vector3, Time.deltaTime * speed);
 
         float lx = size.x * 0.5f - width;
         float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
