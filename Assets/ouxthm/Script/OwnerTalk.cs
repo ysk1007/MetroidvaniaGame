95c2c59ba0c6f260e376146846e9fbb12b7bf544
OuxtHm <blue29856@naver.com>
2023-08-15 21:15:46 +0900

    Modifying OwnerTalk script
    대화창 불러오는 변수 MarketScript에서 받아옴. (이전에 있던 오류 해결)

--- Assets/ouxthm/Script/OwnerTalk.cs
+++ Assets/ouxthm/Script/OwnerTalk.cs
@@ -7,51 +7,41 @@ using UnityEngine.UI;
 [System.Serializable]
 public class Dialogue 
 {
-    
     public string name;
     [TextArea]
     public string dialogue;
     public Image poImage;
 }
 
-
 public class OwnerTalk : MonoBehaviour
 {
-    MarketScript market;
-    [SerializeField] private GameObject blackBox; // 검은 배경, 검은 대화박스 부모 오브젝트
-    [SerializeField] private GameObject portrait; // 초상화 icon
-    [SerializeField] private TextMeshProUGUI txt_name;    // 초상화 이름
-    [SerializeField] private TextMeshProUGUI txt_dialogue;    // 대화 내용
-    [SerializeField] private TextMeshProUGUI txt_Enter;     // Enter 안내 문구
+    MarketScript market;                                       
+    public GameObject allObject;   // 모든 오브젝트
+    public GameObject portrait; // 초상화 icon
+    public TextMeshProUGUI txt_name;    // 초상화 이름
+    public TextMeshProUGUI txt_dialogue;    // 대화 내용
+    public TextMeshProUGUI txt_Enter;     // Enter 안내 문구
+
     public bool isDialogue; 
     public bool isShow;     // 오브젝트 켜는 변수
     private int count = 0;
     
-    [SerializeField] private Dialogue[] dialogue;
+    public Dialogue[] dialogue;
 
-    public void ShowDialogue()
+    public void ShowDialogue()  // UI
     {
-        blackBox.SetActive(true);
-        portrait.SetActive(true);
-        txt_name.gameObject.SetActive(true);
-        txt_dialogue.gameObject.SetActive(true);
-        txt_Enter.gameObject.SetActive(true);
-
-        count = 0;
         isDialogue = false;
+        allObject.SetActive(true);  // 모든 오브젝트 ON
+        count = 0;
         NextDialogue();
     }
 
     private void HideDialogue()
     {
-        blackBox.gameObject.SetActive(false);
-        portrait.gameObject.SetActive(false);
-        txt_name.gameObject.SetActive(false);
-        txt_dialogue.gameObject.SetActive(false);
-        txt_Enter.gameObject.SetActive(false);
+        allObject.SetActive(false); // 모든 오브젝트 OFF
     }
 
-    private void NextDialogue()
+    private void NextDialogue() // 다음 대화 
     {
         txt_name.text = dialogue[count].name;
         txt_dialogue.text = dialogue[count].dialogue;
@@ -60,15 +50,15 @@ public class OwnerTalk : MonoBehaviour
     }
     void Awake()
     {
-        market = MarketScript.instance.GetComponent<MarketScript>();
+        market = MarketScript.instance.GetComponent<MarketScript>();   
         isDialogue = true;
     }
     void Update()
     {
-        isShow = market.PlayerVisit;
+        isShow = market.PlayerVisit;  
         if (isShow)
         {
-            if (isDialogue) 
+            if (isDialogue)
             {
                 ShowDialogue();
             }
