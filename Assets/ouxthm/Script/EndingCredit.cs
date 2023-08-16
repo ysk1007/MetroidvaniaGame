e42c7f4bd767113b40f1de3ee38ca55c620efb6a
OuxtHm <blue29856@naver.com>
2023-08-16 20:53:10 +0900

    Modifying the EndingCredit.cs
    엔딩 크레딧이 끝나면 타이틀로 씬 이동
    
    스킵 기능 추가
    스킵하여 바로 타이틀 화면으로 갈 수 있음

--- Assets/ouxthm/Script/EndingCredit.cs
+++ Assets/ouxthm/Script/EndingCredit.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndingCredit : MonoBehaviour
 {
-
+    public GameObject skipButton;   // 스킵 이미지와 텍스트가 들어있는 오브젝트
     public GameObject credit;   // 실질적 크레딧
     public Image Panel;     // 검은 박스
 
-    public bool isEnding;       // 엔딩인지 확인하는 변수
+    public bool isEnding = false;       // 엔딩인지 확인하는 변수
+    public bool isSkip = false;   // 스킵
+    public bool isShow = false;
     float time = 0f;
     float F_time = 30f;
 
@@ -17,13 +20,27 @@ public class EndingCredit : MonoBehaviour
     {
         if (isEnding)
         {
-            StartCoroutine(FadeFlow());
+            FadeFlow();
             credit.SetActive(true);
+            StartCoroutine(GoTitleScene());
+            if (!isShow)
+            {
+                Invoke("ShowSkipButton", 10f);
+            }
+            else if (isShow)
+            {
+                GoSkip();
+            }
+
+            if (isSkip)
+            {
+                credit.SetActive(false);
+                SkipTitleScene();
+            }
+
         }
-
     }
-
-    IEnumerator FadeFlow()
+    void FadeFlow()
     {
         Color alpha = Panel.color;
         while (alpha.a < 1f)
@@ -31,9 +48,28 @@ public class EndingCredit : MonoBehaviour
             time += Time.deltaTime / F_time;
             alpha.a = Mathf.Lerp(0, 1, time);
             Panel.color = alpha;
-            yield return null;
         }
+    }
+    IEnumerator GoTitleScene()
+    {
+        yield return new WaitForSeconds(60f);
+        SceneManager.LoadScene("Title_Scene");
+    }
+    public void SkipTitleScene()
+    {
+        SceneManager.LoadScene("Title_Scene");
+    }
 
-        yield return null;
+    public void ShowSkipButton()
+    {
+        skipButton.SetActive(true);
+        isShow = true;
+    }
+    public void GoSkip()
+    {
+        if(isShow && Input.GetKeyDown(KeyCode.Return))
+        {
+            isSkip = true;
+        }
     }
 }
